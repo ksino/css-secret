bf070449c865a7e3ac6cccfb2110066257c93495
ksino <flycat86@163.com>
2019-05-15 05:55:53 +0800

    4测试js文件

---
+++ js/utils.js
@@ -0,0 +1,23 @@
+var log = function() {
+    console.log(console, arguments)
+}
+
+var q = function(e) {
+    return document.querySelector(e)
+}
+
+var qa = function(e) {
+    return document.querySelectorAll(e)
+}
+
+var toggle = function(name) {
+    var show = q('.show')
+    window.addEventListener('click', (e) => {
+        log(e.target)
+        if (show.classList.contains(name)) {
+            show.classList.remove(name)
+        } else {
+            show.classList.add(name)
+        }
+    })
+}
